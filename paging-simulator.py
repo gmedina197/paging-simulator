277ccc98a777e31db4b43ec4050147bd458aacb5
Gabriel Medina <gmedina197@gmail.com>
2018-07-26 23:56:56 -0300

    initializing hash of pages

--- paging-simulator.py
+++ paging-simulator.py
@@ -1,14 +1,5 @@
 import time
-
-def load_anim():
-    print("|", end="\r")
-    time.sleep(0.5)
-    print("/", end="\r")
-    time.sleep(0.5)
-    print("-", end="\r")
-    time.sleep(0.5)
-    print("\\", end="\r")
-    time.sleep(0.5)
+from collections import defaultdict
 
 def hex2bin(hexdata):
     scale = 16 
@@ -16,15 +7,31 @@ def hex2bin(hexdata):
     b = bin(int(hexdata, scale))[2:].zfill(num_of_bits)
     return b
 
-def readFile(page_size):
+def readFile(page_size, path_file):
     d = {}
     count = 1
-    with open('/home/medina/UNIOESTE/SO/traces/gcc.trace') as f:
+    with open(path_file) as f:
         for line in f:
             (key, val) = line.split()
             d[count] = hex2bin(key)[:page_size]
             count += 1
     return d
+
+def hashFile(page_size, path_file):
+    d = defaultdict(list)
+    a = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2]
+    tracker = 0
+    for idx, value in enumerate(a):
+        d[value].append(tracker)
+        tracker += 1
+        """if value in d.keys():
+            d[value].append(tracker)
+            tracker += 1
+        else:
+            d[value].append(tracker)
+            tracker += 1"""
+
+    print(d.items())
 
 def predict(frames, processList, idx):
     res = -1
@@ -60,8 +67,8 @@ def optimal(processList, frames_size):
             frames[replace] = page
             misses += 1
         
-    print('n hits = ' + str(hits))
-    print('n misses c= ' + str(misses))  
+    print('n hits = %s' % hits)
+    print('n misses = %s' % misses) 
 
 def aprox_LRU(processList, frames_size):
     pass
@@ -72,10 +79,11 @@ frames_size = 4
 page_size = 16
 
 start_time = time.time()
-#processList = readFile(page_size)
-processList = {0:7, 1:0, 2:1, 3:2, 4:0, 5:3, 6:0,7:4, 8:2, 9:3, 10:0, 11:3, 12:2 }
+#processList = readFile(page_size, '/home/medina/UNIOESTE/SO/traces/gcc.trace')
+processList = { 0:7, 1:0, 2:1, 3:2, 4:0, 5:3, 6:0, 7:4, 8:2, 9:3, 10:0, 11:3, 12:2 }
+hashFile(page_size, '/home/medina/UNIOESTE/SO/traces/gcc.trace')
 
-print("--------------------Algoritmo Otimo-----------------------")
+""" print("--------------------Algoritmo Otimo-----------------------")
 optimal(processList, frames_size)
 print("--------------------LRU Aproximado-----------------------")
 aprox_LRU(processList, frames_size)
@@ -84,5 +92,5 @@ exec_time = (time.time() - start_time)
 if exec_time > 60:
     print("---Execução: %s minutos ---" % ( exec_time / 60))
 else:
-    print("---Execução: %s segundos ---" % exec_time)
+    print("---Execução: %s segundos ---" % exec_time) """
 
